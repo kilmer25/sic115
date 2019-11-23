8b88abc095df33989ef0851653bc07dbc77a9816
kyl25 <ml16006@ues.edu.sv>
2019-11-23 11:53:07 -0600

    Primer comit

---
+++ SicProyecto.Module/BusinessObjects/ChocoloatCode/Parametros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace SicProyecto.Module.BusinessObjects.Chocoloat
+{
+
+    public partial class Parametros
+    {
+        public Parametros(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ SicProyecto.Module/BusinessObjects/SicEnum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SicProyecto.Module.BusinessObjects
+{
+    public class SicEnum
+    {
+
+        public enum RubrosContables
+        {
+            Pasivo,
+            Activo,
+            Patrimonio,
+            Ingresos,
+            Gastos,
+            Resultados
+        }
+        public enum Saldo
+        {
+            Deudor,
+            Acreedor
+        }
+        public enum TipoCuenta
+        {
+            Activo,
+            Pasivo,
+            Patrimonio,
+            Ingreso,
+            Gasto,
+            Resultado
+        }
+        public enum EstadoPartidaContable
+        {
+            Borrador = 0,
+            Mayorizada = 1,
+            Anulada = 2
+        }
+        public enum TipoPartida
+        {
+            Ninguno = 0,
+            Liquidacion = 1,
+            CierreAnual = 2
+        }
+        public enum TipoDetalleEstadoFinanciero
+        {
+            Ingresos = 0,
+            RebajasYDevoluciones = 1,
+            CostoDeVenta = 2,
+            Gastos = 3,
+            GastosFinancieros = 4,
+            OtrosIngresos = 5
+        }
+    }
+}
