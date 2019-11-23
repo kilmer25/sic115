28f13dfb9cd0a4a3bc6f7ae05ac8b510ef8e1f80
kyl25 <ml16006@ues.edu.sv>
2019-11-22 21:18:36 -0600

    Agregar archivos de proyecto.

---
+++ SicProyecto.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace SicProyecto.Module.Win {
+    partial class SicProyectoWindowsFormsModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // SicProyectoWindowsFormsModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(SicProyecto.Module.SicProyectoModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+			this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule));
+        }
+
+        #endregion
+    }
+}
